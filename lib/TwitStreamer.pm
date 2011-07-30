b5b648f8ab98e05c9ed47419c9bb6da0e47eab9b
Motonori Iwata <iwata.motonori@dena.jp>
2011-07-30 19:21:57 +0900

    initial commit

---
+++ lib/TwitStreamer.pm
@@ -0,0 +1,32 @@
+package TwitStreamer;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+TwitStreamer -
+
+=head1 SYNOPSIS
+
+  use TwitStreamer;
+
+=head1 DESCRIPTION
+
+TwitStreamer is
+
+=head1 AUTHOR
+
+Motonori Iwata E<lt>gootonori@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
